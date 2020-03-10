32a40fed1a04f973e0c2e41cd7907e2fbe8bdc29
Leandro Cosas <lecosas@gmail.com>
2020-03-10 13:37:02 -0500

    Criação da tela de Ajustes e consoa dos Outlets com a SettingsViewController (falta constraints)

---
+++ BestBuy/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  BestBuy
+//
+//  Created by administrator on 3/10/20.
+//  Copyright © 2020 fiap. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tfDollar: UITextField!
+    @IBOutlet weak var tfIOF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func btAddState(_ sender: UIButton) {
+    }
+    
+}
